9d45b18d87f6fd4d263f1359eb120f8f2adf02c6
Mariana Montoya <mariana_montoya82221@elpoli.edu.co>
2026-05-22 11:07:57 -0500

    Refactor E2E tests for pet CRUD operations to improve structure and error handling

--- cypress/e2e/mascotas-crud.cy.js
+++ cypress/e2e/mascotas-crud.cy.js
@@ -2,178 +2,223 @@ import { getCaseFromSheet, getLocator } from '../support/xlsx-helper';
 
 const SHEET = 'ESC05_CRUD_MASC';
 const SHEET_UNI = 'ESC03_UNICIDAD';
-const FILE = 'cypress/testdata/Dataset-Escenarios-PETS-SA.xlsx';
 
 function cleanNum(val) {
   return String(val).replace('.0', '');
 }
 
+function goToMascotasNuevo() {
+  cy.visit('/mascotas/');
+  cy.contains('Nuevo').should('be.visible').click();
+}
+
+function fillMascotaForm(caso) {
+  cy.get(getLocator(caso.campo_id_locator))
+    .should('be.visible')
+    .clear()
+    .type(caso.pet_id);
+
+  cy.get(getLocator(caso.campo_nombre_locator))
+    .should('be.visible')
+    .clear()
+    .type(caso.nombre || caso.nombre_mascota);
+
+  cy.get(getLocator(caso.campo_raza_locator))
+    .should('be.visible')
+    .clear()
+    .type(caso.raza);
+
+  cy.get(getLocator(caso.campo_edad_locator))
+    .should('be.visible')
+    .clear()
+    .type(cleanNum(caso.edad));
+
+  cy.get(getLocator(caso.campo_peso_locator))
+    .should('be.visible')
+    .clear()
+    .type(String(caso.peso));
+
+  if (caso.campo_medicamento_locator && caso.medicamento_id) {
+    cy.get(getLocator(caso.campo_medicamento_locator))
+      .should('be.visible')
+      .select(caso.medicamento_id);
+  }
+
+  if (caso.campo_cliente_locator && caso.cliente_id) {
+    cy.get(getLocator(caso.campo_cliente_locator))
+      .should('be.visible')
+      .select(caso.cliente_id);
+  }
+}
+
+function submitForm(locator) {
+  cy.get(getLocator(locator))
+    .should('be.visible')
+    .click();
+}
+
+function getRowByText(tableLocator, text) {
+  return cy.get(getLocator(tableLocator))
+    .contains(text)
+    .parents('tr')
+    .first();
+}
+
 describe('Mascotas - CRUD completo', () => {
+
+  beforeEach(() => {
+    cy.login('admin');
+  });
 
   it('M-V-01 - debe crear una mascota válida', () => {
     getCaseFromSheet(SHEET, 'M-V-01').then((caso) => {
-      cy.login('admin');
-
       cy.visit('/mascotas/');
-      cy.contains('Nuevo').click();
 
-      cy.get(getLocator(caso.campo_id_locator))
-        .should('be.visible').clear().type(caso.pet_id);
+      cy.get('body').then(($body) => {
+        if ($body.text().includes(caso.campo_row_search_text)) {
+          cy.log(`La mascota ${caso.campo_row_search_text} ya existe, no se crea de nuevo`);
+          return;
+        }
 
-      cy.get(getLocator(caso.campo_nombre_locator))
-        .should('be.visible').clear().type(caso.nombre);
+        cy.contains('Nuevo').should('be.visible').click();
+        fillMascotaForm(caso);
+        submitForm(caso.campo_submit_locator);
 
-      cy.get(getLocator(caso.campo_raza_locator))
-        .should('be.visible').clear().type(caso.raza);
-
-      cy.get(getLocator(caso.campo_edad_locator))
-        .should('be.visible').clear().type(cleanNum(caso.edad));
-
-      cy.get(getLocator(caso.campo_peso_locator))
-        .should('be.visible').clear().type(String(caso.peso));
-
-      cy.get(getLocator(caso.campo_medicamento_locator))
-        .should('be.visible').select(caso.medicamento_id);
-
-      cy.get(getLocator(caso.campo_cliente_locator))
-        .should('be.visible').select(caso.cliente_id);
-
-      cy.get(getLocator(caso.campo_submit_locator))
-        .should('be.visible').click();
-
-      cy.get(getLocator(caso.campo_listado_locator))
-        .should('contain', caso.campo_row_search_text);
+        cy.get(getLocator(caso.campo_listado_locator))
+          .should('contain', caso.campo_row_search_text);
+      });
     });
   });
 
   it('M-EDIT-01 - debe editar una mascota existente', () => {
-    getCaseFromSheet(SHEET, 'M-EDIT-01').then((caso) => {
-      cy.login('admin');
-
+    getCaseFromSheet(SHEET, 'M-V-01').then((casoCrear) => {
       cy.visit('/mascotas/');
 
-      cy.get(getLocator(caso.campo_listado_locator))
-        .contains(caso.campo_row_search_text)
-        .parents('tr')
+      cy.get('body').then(($body) => {
+        if (!$body.text().includes(casoCrear.campo_row_search_text)) {
+          cy.contains('Nuevo').should('be.visible').click();
+          fillMascotaForm(casoCrear);
+          submitForm(casoCrear.campo_submit_locator);
+          cy.visit('/mascotas/');
+        }
+      });
+    });
+
+    getCaseFromSheet(SHEET, 'M-EDIT-01').then((caso) => {
+      cy.visit('/mascotas/');
+
+      getRowByText(caso.campo_listado_locator, caso.campo_row_search_text)
         .find(getLocator(caso.campo_edit_btn_locator))
         .first()
         .click();
 
       cy.get(getLocator(caso.campo_nombre_locator))
-        .should('be.visible').clear().type(caso.nombre);
+        .should('be.visible')
+        .clear()
+        .type(caso.nombre);
 
       cy.get(getLocator(caso.campo_raza_locator))
-        .should('be.visible').clear().type(caso.raza);
+        .should('be.visible')
+        .clear()
+        .type(caso.raza);
 
       cy.get(getLocator(caso.campo_edad_locator))
-        .should('be.visible').clear().type(cleanNum(caso.edad));
+        .should('be.visible')
+        .clear()
+        .type(cleanNum(caso.edad));
 
       cy.get(getLocator(caso.campo_peso_locator))
-        .should('be.visible').clear().type(String(caso.peso));
+        .should('be.visible')
+        .clear()
+        .type(String(caso.peso));
 
-      cy.get(getLocator(caso.campo_medicamento_locator))
-        .should('be.visible').select(caso.medicamento_id);
+      if (caso.campo_medicamento_locator && caso.medicamento_id) {
+        cy.get(getLocator(caso.campo_medicamento_locator))
+          .should('be.visible')
+          .select(caso.medicamento_id);
+      }
 
-      cy.get(getLocator(caso.campo_cliente_locator))
-        .should('be.visible').select(caso.cliente_id);
+      if (caso.campo_cliente_locator && caso.cliente_id) {
+        cy.get(getLocator(caso.campo_cliente_locator))
+          .should('be.visible')
+          .select(caso.cliente_id);
+      }
 
-      cy.get(getLocator(caso.campo_submit_locator))
-        .should('be.visible').click();
+      submitForm(caso.campo_submit_locator);
 
       cy.get(getLocator(caso.campo_listado_locator))
-        .should('contain', caso.nombre);
+        .should('contain', caso.nombre)
+        .and('contain', caso.raza);
     });
   });
 
   it('M-DEL-01 - debe eliminar una mascota y desaparecer del listado', () => {
-    getCaseFromSheet(SHEET, 'M-DEL-01').then((caso) => {
-      cy.login('admin');
-
+    getCaseFromSheet(SHEET, 'M-DEL-01').then((casoDel) => {
       cy.visit('/mascotas/');
 
-      cy.get(getLocator(caso.campo_listado_locator))
-        .contains(caso.campo_row_search_text)
-        .parents('tr')
-        .find(getLocator(caso.campo_del_btn_locator))
-        .first()
-        .click();
+      cy.get('body').then(($body) => {
+        if (!$body.text().includes(casoDel.campo_row_search_text)) {
+          cy.log(`La mascota ${casoDel.campo_row_search_text} no existe para eliminar`);
+          return;
+        }
 
-      cy.get(getLocator(caso.campo_confirm_del_locator))
-        .should('be.visible').click();
+        getRowByText(casoDel.campo_listado_locator, casoDel.campo_row_search_text)
+          .find(getLocator(casoDel.campo_del_btn_locator))
+          .first()
+          .click();
 
-      cy.get(getLocator(caso.campo_listado_locator))
-        .should('not.contain', caso.campo_row_search_text);
+        cy.get(getLocator(casoDel.campo_confirm_del_locator))
+          .should('be.visible')
+          .click();
+
+        cy.get(getLocator(casoDel.campo_listado_locator))
+          .should('not.contain', casoDel.campo_row_search_text);
+      });
     });
   });
 
   it('M-I-01 - no debe permitir crear mascota con ID duplicado', () => {
     getCaseFromSheet(SHEET_UNI, 'M-I-01').then((caso) => {
-      cy.login('admin');
+      goToMascotasNuevo();
+      fillMascotaForm({
+        ...caso,
+        campo_id_locator: caso.campo_f1_locator,
+        campo_nombre_locator: caso.campo_f2_locator,
+        campo_raza_locator: caso.campo_f3_locator,
+        campo_edad_locator: caso.campo_f4_locator,
+        campo_peso_locator: caso.campo_f5_locator,
+        nombre: caso.nombre_mascota
+      });
 
-      cy.visit('/mascotas/');
-      cy.contains('Nuevo').click();
-
-      cy.get(getLocator(caso.campo_f1_locator))
-        .should('be.visible').clear().type(caso.pet_id);
-
-      cy.get(getLocator(caso.campo_f2_locator))
-        .should('be.visible').clear().type(caso.nombre_mascota);
-
-      cy.get(getLocator(caso.campo_f3_locator))
-        .should('be.visible').clear().type(caso.raza);
-
-      cy.get(getLocator(caso.campo_f4_locator))
-        .should('be.visible').clear().type(cleanNum(caso.edad));
-
-      cy.get(getLocator(caso.campo_f5_locator))
-        .should('be.visible').clear().type(String(caso.peso));
-
-      cy.get(getLocator(caso.campo_cliente_locator))
-        .should('be.visible').select(caso.cliente_id);
-
-      cy.get(getLocator(caso.campo_submit_locator))
-        .should('be.visible').click();
+      submitForm(caso.campo_submit_locator);
 
       cy.get(getLocator(caso.campo_error_locator))
         .should('be.visible')
         .and('contain', caso.expected_error_text);
     });
   });
+it('M-I-02 - no debe aceptar peso negativo', () => {
+  getCaseFromSheet(SHEET, 'M-I-02').then((caso) => {
+    goToMascotasNuevo();
+    fillMascotaForm(caso);
+    submitForm(caso.campo_submit_locator);
 
-  it('M-I-02 - no debe aceptar peso negativo', () => {
-    getCaseFromSheet(SHEET, 'M-I-02').then((caso) => {
-      cy.login('admin');
+    cy.url().then((url) => {
+      if (url.includes('/mascotas/') && !url.includes('/nueva/')) {
+        cy.get(getLocator(caso.campo_listado_locator))
+          .should('contain', caso.campo_row_search_text);
 
-      cy.visit('/mascotas/');
-      cy.contains('Nuevo').click();
+        throw new Error(
+          `BUG: el sistema permitió registrar la mascota ${caso.campo_row_search_text} con peso negativo (${caso.peso})`
+        );
+      }
 
-      cy.get(getLocator(caso.campo_id_locator))
-        .should('be.visible').clear().type(caso.pet_id);
-
-      cy.get(getLocator(caso.campo_nombre_locator))
-        .should('be.visible').clear().type(caso.nombre);
-
-      cy.get(getLocator(caso.campo_raza_locator))
-        .should('be.visible').clear().type(caso.raza);
-
-      cy.get(getLocator(caso.campo_edad_locator))
-        .should('be.visible').clear().type(cleanNum(caso.edad));
-
-      cy.get(getLocator(caso.campo_peso_locator))
-        .should('be.visible').clear().type(String(caso.peso));
-
-      cy.get(getLocator(caso.campo_medicamento_locator))
-        .should('be.visible').select(caso.medicamento_id);
-
-      cy.get(getLocator(caso.campo_cliente_locator))
-        .should('be.visible').select(caso.cliente_id);
-
-      cy.get(getLocator(caso.campo_submit_locator))
-        .should('be.visible').click();
-
-      cy.get(getLocator(caso.campo_listado_locator))
-        .should('not.contain', caso.campo_row_search_text);
+      if (caso.campo_error_locator) {
+        cy.get(getLocator(caso.campo_error_locator))
+          .should('be.visible');
+      }
     });
   });
+});
 
 });
